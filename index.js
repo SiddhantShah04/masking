629673d3c206cff43a92d3177ac43f31f015d366
siddhantshah04 <you@example.com>
2022-09-25 19:17:08 +0530

    file changes

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const multer = require("multer");
 // const upload = multer({ dest: "uploads/" });
 const path = require("path");
 const { spawn } = require("child_process");
+const fs = require("fs");
 
-const PORT =5000
+const PORT = 5000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+function runPythonScript() {
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn("python", ["dataMasking.py"]);
+  // collect data from script
+  return new Promise((resolve, reject) => {
+    python.stdout.on("data", function (data) {
+      console.log("Pipe data from python script ...");
+      dataToSend = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on("close", (code) => {
+      // console.log(`child process close all stdio with code ${code}`);
+      // send data to browser
+      var buffer = fs.readFileSync("data/output/news.txt");
+      resolve(buffer.toString());
+    });
+  });
+}
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -54,27 +75,23 @@ var upload = multer({
 
 app.post("/upload_files", upload.array("files"), uploadFiles);
 
+app.post("/input_text", async (req, res) => {
+  // var path = process.cwd();
+
+  const { text } = req.body;
+  fs.writeFileSync("data/input/news.txt", text);
+  const masked = await runPythonScript();
+  res.send(masked);
+});
+
 app.get("/", (req, res) => {
   res.send("welcome");
 });
 
-function uploadFiles(req, res) {
-
-  var dataToSend;
-  // spawn new child process to call the python script
-  const python = spawn("python", ["dataMasking.py"]);
-  // collect data from script
-  python.stdout.on("data", function (data) {
-    console.log("Pipe data from python script ...");
-    dataToSend = data.toString();
-  });
-  // in close event we are sure that stream from child process is closed
-  python.on("close", (code) => {
-    // console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send(dataToSend);
-  });
+async function uploadFiles(req, res) {
+  const text = await runPythonScript();
+  res.send(text);
 }
 app.listen(PORT, () => {
-  console.log(`Server started runnig on`,PORT);
+  console.log(`Server started runnig on`, PORT);
 });
